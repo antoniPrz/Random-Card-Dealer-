ed5b2c87f45aee557ef2a680c97fbf05df2f5813
antoni perez <antoni.protoni@gmail.com>
2021-02-28 22:03:59 -0300

    set 2 random pintas

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 let numerosCard = [ "A",1,2,3,4,5,6,7,8,9,10,"J","Q","K"];
 let numerosCardIndex = Math.floor(Math.random()*numerosCard.length);
-let carValue = document.getElementById("numero")
+let cardValue = document.getElementById("numero")
 
 let num = document.createTextNode("10");
 let numRamdom = document.createTextNode(  numerosCard[numerosCardIndex]  ) ;
-carValue.appendChild(numRamdom);
+cardValue.appendChild(numRamdom);
 
 
 
-let pintas = [ ]
+let pintas = ['♦','♥','♠','♣'];
+let pintaIndx = Math.floor(Math.random()*pintas.length);
+let pinta1 = document.getElementById("pinta-1") 
+let pinta2 = document.getElementById("pinta-2") 
+
+
+let pintaRandom = document.createTextNode(pintas[pintaIndx]);
+let pintaRandom2 = document.createTextNode(pintas[pintaIndx]);
+pinta1.appendChild(pintaRandom);
+pinta2.appendChild(pintaRandom2);
+
 
 
 
