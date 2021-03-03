da597f076758f70739e3c082134b5bc052ed071a
antoni perez <antoni.protoni@gmail.com>
2021-03-02 22:46:07 -0300

    numberon listo, tipon de pintas listo

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ cardValue.appendChild(numRamdom);
 
 let pintas = ['♦','♥','♠','♣'];
 let pintaIndx = Math.floor(Math.random()*pintas.length);
+
 let pinta1 = document.getElementById("pinta-1") 
 let pinta2 = document.getElementById("pinta-2") 
 
 
+
 let pintaRandom = document.createTextNode(pintas[pintaIndx]);
 let pintaRandom2 = document.createTextNode(pintas[pintaIndx]);
+
+// pintaRandom = pintaRandom.style.color="red";
+
 pinta1.appendChild(pintaRandom);
 pinta2.appendChild(pintaRandom2);
 
