a94259feb29d48f4a8696003e3ad78038908645c
antoni perez <antoni.protoni@gmail.com>
2021-03-03 16:57:24 -0300

    color red listo

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-let numerosCard = [ "A",1,2,3,4,5,6,7,8,9,10,"J","Q","K"];
-let numerosCardIndex = Math.floor(Math.random()*numerosCard.length);
-let cardValue = document.getElementById("numero")
+let numerosCard = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let numerosCardIndex = Math.floor(Math.random() * numerosCard.length);
+let cardValue = document.getElementById("numero");
 
 let num = document.createTextNode("10");
-let numRamdom = document.createTextNode(  numerosCard[numerosCardIndex]  ) ;
+let numRamdom = document.createTextNode(numerosCard[numerosCardIndex]);
 cardValue.appendChild(numRamdom);
 
+let pintas = ["♦", "♥", "♠", "♣"];
+let pintaIndx = Math.floor(Math.random() * pintas.length);
 
+let pinta1 = document.getElementById("pinta-1");
+let pinta2 = document.getElementById("pinta-2");
 
-let pintas = ['♦','♥','♠','♣'];
-let pintaIndx = Math.floor(Math.random()*pintas.length);
+let pintaRandom = pintas[pintaIndx];
 
-let pinta1 = document.getElementById("pinta-1") 
-let pinta2 = document.getElementById("pinta-2") 
+function pintaR() {
+  if (pintaRandom == "♦" || pintaRandom == "♥") {
+    pintaRandom = document.getElementById("pinta-1").style.color = "red";
+    pintaRandom = document.getElementById("pinta-2").style.color = "red";
+  } else {
+    pintaRandom = document.getElementById("pinta-1").style.color = "black";
+    pintaRandom = document.getElementById("pinta-2").style.color = "black";
+  }
 
+  pintaRandom = pintas[pintaIndx];
 
+  return pintaRandom;
+}
 
-let pintaRandom = document.createTextNode(pintas[pintaIndx]);
-let pintaRandom2 = document.createTextNode(pintas[pintaIndx]);
-
-// pintaRandom = pintaRandom.style.color="red";
-
-pinta1.appendChild(pintaRandom);
-pinta2.appendChild(pintaRandom2);
-
-
-
-
-
+pinta1.append(pintaR());
+pinta2.append(pintas[pintaIndx]);
